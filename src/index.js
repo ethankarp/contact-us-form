ed4f782ddc93188b1f901bf37845ac834978f6dc
Ethan Karp <ethansmainemail@gmail.com>
2022-01-23 14:55:11 -0700

    created index.js

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import ContactForm from './components/ContactForm'
+
+ReactDOM.render(
+  <ContactForm />,
+  document.getElementById('root')
+)
